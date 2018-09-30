167551d333252e7b873be56ae4cd658154c34a48
Nishant Jain <nishantkumarajain@gmail.com>
2018-09-30 22:29:28 +0530

    Added routing. Changed the docker-compose file, added labels for service-discovery. Updated package.json to serve on all interfaces and serve with base-href 'ontology'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ImportConceptMapComponent } from './import-concept-map/import-concept-map.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { MatStepperModule,  MatAutocompleteModule } from '@angular/material';
+import { RouterModule, Routes, Router } from '@angular/router';
 import * as d3 from "d3";
 import {
   MatCardModule,
@@ -22,12 +24,26 @@ import {
   MatTableModule,
 } from '@angular/material';
 
+const appRoutes: Routes = [
+  {
+    path: 'import-concept-map',
+    component: ImportConceptMapComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/import-concept-map',
+    pathMatch: 'full',
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
+    ImportConceptMapComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     MatButtonModule,
     MatToolbarModule,
     MatDialogModule,
