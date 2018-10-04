608ed9a3892035fe7a064cd44ef64f98f2989ce7
Preety Kumari <preetydakshana@gmail.com>
2018-10-04 18:15:14 +0530

    Updated Graph

--- src/app/import-concept-map/import-concept-map.component.ts
+++ src/app/import-concept-map/import-concept-map.component.ts
@@ -235,8 +235,8 @@ export class ImportConceptMapComponent {
 
     function ticked() {
       link
-        .attr('x1', function (d) { return d['source'].x+100; })
-        .attr('y1', function (d) { return d['source'].y+100; })
+        .attr('x1', function (d) { return d['source'].x; })
+        .attr('y1', function (d) { return d['source'].y; })
         .attr('x2', function (d) { return d['target'].x; })
         .attr('y2', function (d) { return d['target'].y; });
 
